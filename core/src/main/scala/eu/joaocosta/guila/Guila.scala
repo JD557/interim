fef965731203adaeb239fd21fa63886f6107b401
João Costa <jdpc557@gmail.com>
2023-06-05 23:14:42 +0200

    Add a return value to components and layouts

--- core/src/main/scala/eu/joaocosta/guila/Guila.scala
+++ core/src/main/scala/eu/joaocosta/guila/Guila.scala
@@ -13,9 +13,11 @@ object Guila:
         uiState.activeItem = Some(id)
     (uiState.hotItem == Some(id), uiState.activeItem == Some(id))
 
-  def window(inputState: InputState, uiState: UiState)(
-      run: (inputState: InputState, uiState: UiState) ?=> Unit
-  ): (List[RenderOp], UiState) =
+  type Component[+T] = (inputState: InputState, uiState: UiState) ?=> T
+
+  def window[T](inputState: InputState, uiState: UiState)(
+      run: (inputState: InputState, uiState: UiState) ?=> T
+  ): (List[RenderOp], UiState, T) =
     val nextState = uiState.clone()
     // prepare
     nextState.ops.clear()
@@ -23,13 +25,13 @@ object Guila:
     // run
     given is: InputState = inputState
     given us: UiState    = nextState
-    run
+    val res              = run
     // finish
     if (!inputState.mouseDown) nextState.activeItem = None
     // return
-    (nextState.ops.toList, nextState)
+    (nextState.ops.toList, nextState, res)
 
-  def grid(area: Rect, rows: Int, columns: Int, padding: Int)(body: Iterator[Rect] => Unit): Unit =
+  def grid[T](area: Rect, rows: Int, columns: Int, padding: Int)(body: Iterator[Rect] => T): T =
     val rowSize    = (area.h - (rows - 1) * padding) / rows
     val columnSize = (area.w - (columns - 1) * padding) / columns
     val it = for
@@ -46,40 +48,36 @@ object Guila:
   def text(area: Rect, text: String, color: Color, center: Boolean = false)(implicit uiState: UiState): Unit =
     if (text.nonEmpty) uiState.ops.addOne(RenderOp.DrawText(area, text, color, center))
 
-  def button(id: ItemId, area: Rect, label: String = "", skin: ButtonSkin = ButtonSkin.Default())(implicit
-      inputState: InputState,
-      uiState: UiState
-  ): Boolean =
+  def button(id: ItemId, area: Rect, label: String = "", skin: ButtonSkin = ButtonSkin.Default()): Component[Boolean] =
     val buttonArea    = skin.buttonArea(area)
     val (hot, active) = setHotActive(id, buttonArea)
     skin.renderButton(area, label, hot, active)
-    hot && active && inputState.mouseDown == false
+    hot && active && summon[InputState].mouseDown == false
 
-  def checkbox(id: ItemId, area: Rect, skin: CheckboxSkin = CheckboxSkin.Default())(value: Boolean)(implicit
-      inputState: InputState,
-      uiState: UiState
-  ): Boolean =
+  def checkbox(id: ItemId, area: Rect, skin: CheckboxSkin = CheckboxSkin.Default())(
+      value: Boolean
+  ): Component[Boolean] =
     val checkboxArea  = skin.checkboxArea(area)
     val (hot, active) = setHotActive(id, checkboxArea)
     skin.renderCheckbox(area, value, hot, active)
-    if (hot && active && inputState.mouseDown == false) !value
+    if (hot && active && summon[InputState].mouseDown == false) !value
     else value
 
   def slider(id: ItemId, area: Rect, skin: SliderSkin = SliderSkin.Default())(
       value: Int,
       max: Int
-  )(implicit inputState: InputState, uiState: UiState): Int =
+  ): Component[Int] =
     val sliderArea    = skin.sliderArea(area)
     val sliderSize    = skin.sliderSize
     val (hot, active) = setHotActive(id, sliderArea)
     skin.renderSlider(area, value, max, hot, active)
     if (active)
       if (area.w > area.h)
-        val mousePos = inputState.mouseX - sliderArea.x - sliderSize / 2
+        val mousePos = summon[InputState].mouseX - sliderArea.x - sliderSize / 2
         val maxPos   = sliderArea.w - sliderSize
         math.max(0, math.min((mousePos * max) / maxPos, max))
       else
-        val mousePos = inputState.mouseY - sliderArea.y - sliderSize / 2
+        val mousePos = summon[InputState].mouseY - sliderArea.y - sliderSize / 2
         val maxPos   = sliderArea.h - sliderSize
         math.max(0, math.min((mousePos * max) / maxPos, max))
     else value
