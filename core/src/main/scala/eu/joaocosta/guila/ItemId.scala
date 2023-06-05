896441d299b320e8d6b4d73e0e11400583b7e140
João Costa <jdpc557@gmail.com>
2023-06-05 23:06:06 +0200

    Allow ItemId composition

--- core/src/main/scala/eu/joaocosta/guila/ItemId.scala
+++ core/src/main/scala/eu/joaocosta/guila/ItemId.scala
@@ -1,3 +1,16 @@
 package eu.joaocosta.guila
 
-type ItemId = Int
+type ItemId = (Int | String) | List[(Int | String)]
+
+object ItemId:
+  extension (itemId: ItemId)
+    def toIdList: List[(Int | String)] =
+      itemId match {
+        case int: Int                   => List(int)
+        case str: String                => List(str)
+        case list: List[(Int | String)] => list
+      }
+
+  extension (parentId: ItemId)
+    def |>(childId: ItemId): ItemId =
+      parentId.toIdList ++ childId.toIdList
