4e09a713d9b6be6a2eadbd91820fefe6e88f8af1
João Costa <jdpc557@gmail.com>
2023-06-05 23:05:54 +0200

    Rename text to label

--- core/src/main/scala/eu/joaocosta/guila/Guila.scala
+++ core/src/main/scala/eu/joaocosta/guila/Guila.scala
@@ -46,13 +46,13 @@ object Guila:
   def text(area: Rect, text: String, color: Color, center: Boolean = false)(implicit uiState: UiState): Unit =
     if (text.nonEmpty) uiState.ops.addOne(RenderOp.DrawText(area, text, color, center))
 
-  def button(id: ItemId, area: Rect, text: String = "", skin: ButtonSkin = ButtonSkin.Default())(implicit
+  def button(id: ItemId, area: Rect, label: String = "", skin: ButtonSkin = ButtonSkin.Default())(implicit
       inputState: InputState,
       uiState: UiState
   ): Boolean =
     val buttonArea    = skin.buttonArea(area)
     val (hot, active) = setHotActive(id, buttonArea)
-    skin.renderButton(area, text, hot, active)
+    skin.renderButton(area, label, hot, active)
     hot && active && inputState.mouseDown == false
 
   def checkbox(id: ItemId, area: Rect, skin: CheckboxSkin = CheckboxSkin.Default())(value: Boolean)(implicit
