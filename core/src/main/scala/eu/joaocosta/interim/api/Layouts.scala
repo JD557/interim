8b25b52e62734bfa9e8792483001c0c939d581fe
João Costa <jdpc557@gmail.com>
2023-06-06 23:01:34 +0200

    Support dynamic layouts

--- core/src/main/scala/eu/joaocosta/interim/api/Layouts.scala
+++ core/src/main/scala/eu/joaocosta/interim/api/Layouts.scala
@@ -23,3 +23,29 @@ trait Layouts:
       dx = column * (columnSize + padding)
     yield Rect(area.x + dx, area.y, columnSize, area.h)
     body(vec.toVector)
+
+  final def dynamicRows[T](area: Rect, padding: Int)(body: (Int => Rect) => T): T =
+    var remainingSize = area.h
+    def generateRect(height: Int): Rect =
+      val y = area.y + area.h - remainingSize
+      if (remainingSize >= height + padding)
+        remainingSize -= (height + padding)
+        area.copy(y = y, h = height)
+      else
+        val remainders = remainingSize
+        remainingSize = 0
+        area.copy(y = y, h = remainingSize)
+    body(generateRect)
+
+  final def dynamicColumns[T](area: Rect, padding: Int)(body: (Int => Rect) => T): T =
+    var remainingSize = area.w
+    def generateRect(width: Int): Rect =
+      val x = area.x + area.w - remainingSize
+      if (remainingSize >= width + padding)
+        remainingSize -= (width + padding)
+        area.copy(x = x, w = width)
+      else
+        val remainders = remainingSize
+        remainingSize = 0
+        area.copy(x = x, w = remainingSize)
+    body(generateRect)
