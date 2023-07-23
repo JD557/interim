549e497e8f69e2e1da37ea851923baa50903856c
João Costa <jdpc557@gmail.com>
2023-07-23 17:05:49 +0200

    Add ComponentWithValue

--- core/src/main/scala/eu/joaocosta/interim/api/Components.scala
+++ core/src/main/scala/eu/joaocosta/interim/api/Components.scala
@@ -13,6 +13,15 @@ trait Components:
 
   type Component[+T] = (inputState: InputState, uiState: UiState) ?=> T
 
+  trait ComponentWithValue[T] {
+    def apply(value: Ref[T]): Component[T]
+    def apply(value: T): Component[T] =
+      apply(Ref(value))
+    inline def applyUnion(value: T | Ref[T]): Component[T] = inline value match
+      case x: T      => apply(x)
+      case x: Ref[T] => apply(x)
+  }
+
   /** Button component. Returns true if it's being clicked, false otherwise.
     *
     * @param label text label to show on this button
@@ -30,14 +39,15 @@ trait Components:
 
   /** Checkbox component. Returns true if it's enabled, false otherwise.
     */
-  final def checkbox(id: ItemId, area: Rect, skin: CheckboxSkin = CheckboxSkin.default())(
-      value: Boolean | Ref[Boolean]
-  ): Component[Boolean] =
-    val checkboxArea = skin.checkboxArea(area)
-    val itemStatus   = UiState.registerItem(id, checkboxArea)
-    skin.renderCheckbox(area, Ref.get(value), itemStatus)
-    if (itemStatus.hot && itemStatus.active && summon[InputState].mouseDown == false) Ref.modify(value, v => !v)
-    else Ref.get(value)
+  final def checkbox(id: ItemId, area: Rect, skin: CheckboxSkin = CheckboxSkin.default()): ComponentWithValue[Boolean] =
+    new ComponentWithValue[Boolean]:
+      def apply(value: Ref[Boolean]): Component[Boolean] =
+        val checkboxArea = skin.checkboxArea(area)
+        val itemStatus   = UiState.registerItem(id, checkboxArea)
+        skin.renderCheckbox(area, Ref.get[Boolean](value), itemStatus)
+        if (itemStatus.hot && itemStatus.active && summon[InputState].mouseDown == false)
+          Ref.modify[Boolean](value, v => !v)
+        else Ref.get(value)
 
   /** Radio button component. Returns value currently selected.
     *
@@ -50,51 +60,64 @@ trait Components:
       buttonIndex: Int,
       label: String,
       skin: ButtonSkin = ButtonSkin.default()
-  )(value: Int | Ref[Int]): Component[Int] =
-    val buttonArea = skin.buttonArea(area)
-    val itemStatus = UiState.registerItem(id, buttonArea)
-    val newValue =
-      if (itemStatus.hot && itemStatus.active && summon[InputState].mouseDown == false) Ref.set[Int](value, buttonIndex)
-      else Ref.get[Int](value)
-    if (newValue == buttonIndex) skin.renderButton(area, label, itemStatus.copy(hot = true, active = true))
-    else (skin.renderButton(area, label, itemStatus))
-    newValue
+  ): ComponentWithValue[Int] =
+    new ComponentWithValue[Int]:
+      def apply(value: Ref[Int]): Component[Int] =
+        val buttonArea = skin.buttonArea(area)
+        val itemStatus = UiState.registerItem(id, buttonArea)
+        val newValue =
+          if (itemStatus.hot && itemStatus.active && summon[InputState].mouseDown == false)
+            Ref.set[Int](value, buttonIndex)
+          else Ref.get[Int](value)
+        if (newValue == buttonIndex) skin.renderButton(area, label, itemStatus.copy(hot = true, active = true))
+        else (skin.renderButton(area, label, itemStatus))
+        newValue
 
   /** Slider component. Returns the current position of the slider, between min and max.
     *
     * @param min minimum value for this slider
     * @param max maximum value fr this slider
     */
-  final def slider(id: ItemId, area: Rect, min: Int, max: Int, skin: SliderSkin = SliderSkin.default())(
-      value: Int | Ref[Int]
-  ): Component[Int] =
-    val sliderArea   = skin.sliderArea(area)
-    val sliderSize   = skin.sliderSize(area, min, max)
-    val range        = max - min
-    val itemStatus   = UiState.registerItem(id, sliderArea)
-    val clampedValue = math.max(min, math.min(Ref.get[Int](value), max))
-    skin.renderSlider(area, min, clampedValue, max, itemStatus)
-    if (itemStatus.active)
-      if (area.w > area.h)
-        val mousePos = summon[InputState].mouseX - sliderArea.x - sliderSize / 2
-        val maxPos   = sliderArea.w - sliderSize
-        Ref.set(value, math.max(min, math.min(min + (mousePos * range) / maxPos, max)))
-      else
-        val mousePos = summon[InputState].mouseY - sliderArea.y - sliderSize / 2
-        val maxPos   = sliderArea.h - sliderSize
-        Ref.set(value, math.max(min, math.min((mousePos * range) / maxPos, max)))
-    else Ref.get(value)
+  final def slider(
+      id: ItemId,
+      area: Rect,
+      min: Int,
+      max: Int,
+      skin: SliderSkin = SliderSkin.default()
+  ): ComponentWithValue[Int] =
+    new ComponentWithValue[Int]:
+      def apply(value: Ref[Int]): Component[Int] =
+        val sliderArea   = skin.sliderArea(area)
+        val sliderSize   = skin.sliderSize(area, min, max)
+        val range        = max - min
+        val itemStatus   = UiState.registerItem(id, sliderArea)
+        val clampedValue = math.max(min, math.min(Ref.get[Int](value), max))
+        skin.renderSlider(area, min, clampedValue, max, itemStatus)
+        if (itemStatus.active)
+          if (area.w > area.h)
+            val mousePos = summon[InputState].mouseX - sliderArea.x - sliderSize / 2
+            val maxPos   = sliderArea.w - sliderSize
+            Ref.set(value, math.max(min, math.min(min + (mousePos * range) / maxPos, max)))
+          else
+            val mousePos = summon[InputState].mouseY - sliderArea.y - sliderSize / 2
+            val maxPos   = sliderArea.h - sliderSize
+            Ref.set(value, math.max(min, math.min((mousePos * range) / maxPos, max)))
+        else Ref.get(value)
 
   /** Text input component. Returns the current string inputed.
     */
-  final def textInput(id: ItemId, area: Rect, skin: TextInputSkin = TextInputSkin.default())(
-      value: String | Ref[String]
-  ): Component[String] =
-    val textInputArea = skin.textInputArea(area)
-    val itemStatus    = UiState.registerItem(id, textInputArea)
-    skin.renderTextInput(area, Ref.get(value), itemStatus)
-    if (itemStatus.keyboardFocus) Ref.modify(value, summon[InputState].appendKeyboardInput)
-    else Ref.get(value)
+  final def textInput(
+      id: ItemId,
+      area: Rect,
+      skin: TextInputSkin = TextInputSkin.default()
+  ): ComponentWithValue[String] =
+    new ComponentWithValue[String]:
+      def apply(value: Ref[String]): Component[String] =
+        val textInputArea = skin.textInputArea(area)
+        val itemStatus    = UiState.registerItem(id, textInputArea)
+        skin.renderTextInput(area, Ref.get(value), itemStatus)
+        if (itemStatus.keyboardFocus) Ref.modify(value, summon[InputState].appendKeyboardInput)
+        else Ref.get(value)
 
   /** Draggable handle. Returns the moved area.
     *
@@ -103,18 +126,18 @@ trait Components:
     * Instead of using this component directly, it can be easier to use [[eu.joaocosta.interim.api.Panels.window]]
     * with movable = true.
     */
-  final def moveHandle(id: ItemId, area: Rect, skin: HandleSkin = HandleSkin.default())(
-      value: Rect | Ref[Rect]
-  ): Component[Rect] =
-    val handleArea = skin.handleArea(area)
-    val itemStatus = UiState.registerItem(id, handleArea)
-    skin.renderHandle(area, Ref.get(value), itemStatus)
-    if (itemStatus.active)
-      val handleCenterX = handleArea.x + handleArea.w / 2
-      val handleCenterY = handleArea.y + handleArea.h / 2
-      val mouseX        = summon[InputState].mouseX
-      val mouseY        = summon[InputState].mouseY
-      val deltaX        = mouseX - handleCenterX
-      val deltaY        = mouseY - handleCenterY
-      Ref.modify(value, _.move(deltaX, deltaY))
-    else Ref.get(value)
+  final def moveHandle(id: ItemId, area: Rect, skin: HandleSkin = HandleSkin.default()): ComponentWithValue[Rect] =
+    new ComponentWithValue[Rect]:
+      def apply(value: Ref[Rect]): Component[Rect] =
+        val handleArea = skin.handleArea(area)
+        val itemStatus = UiState.registerItem(id, handleArea)
+        skin.renderHandle(area, Ref.get(value), itemStatus)
+        if (itemStatus.active)
+          val handleCenterX = handleArea.x + handleArea.w / 2
+          val handleCenterY = handleArea.y + handleArea.h / 2
+          val mouseX        = summon[InputState].mouseX
+          val mouseY        = summon[InputState].mouseY
+          val deltaX        = mouseX - handleCenterX
+          val deltaY        = mouseY - handleCenterY
+          Ref.modify(value, _.move(deltaX, deltaY))
+        else Ref.get(value)
