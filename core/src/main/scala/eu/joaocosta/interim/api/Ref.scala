43b6efabc9a2c53ff9da1735d25d6ca49ed05422
João Costa <jdpc557@gmail.com>
2023-07-30 21:22:24 +0200

    Add withRefs/asRefs

--- core/src/main/scala/eu/joaocosta/interim/api/Ref.scala
+++ core/src/main/scala/eu/joaocosta/interim/api/Ref.scala
@@ -1,5 +1,7 @@
 package eu.joaocosta.interim.api
 
+import scala.deriving.Mirror
+
 /** A mutable reference to a variable.
   *
   * When a function receives a Ref as an argument, it will probably mutate it.
@@ -47,8 +49,32 @@ object Ref:
     block(ref)
     ref.value
 
+  /** Destructures an object into a tuple of Refs that can be used inside the block.
+    *  In the end, a new object is returned with the updated values
+    *
+    * Useful to set temporary mutable variables.
+    */
+  def withRefs[T <: Product](initialValue: T)(using mirror: Mirror.ProductOf[T])(
+      block: Tuple.Map[mirror.MirroredElemTypes, Ref] => Unit
+  ): T =
+    val tuple: mirror.MirroredElemTypes      = Tuple.fromProductTyped(initialValue)
+    val refTuple: Tuple.Map[tuple.type, Ref] = tuple.map([T] => (x: T) => Ref(x))
+    block(refTuple.asInstanceOf)
+    type UnRef[T] = T match { case Ref[a] => a }
+    val updatedTuple: mirror.MirroredElemTypes =
+      refTuple.map([T] => (x: T) => x.asInstanceOf[Ref[_]].value.asInstanceOf[UnRef[T]]).asInstanceOf
+    mirror.fromTuple(updatedTuple)
+
   /** Wraps this value into a Ref and passes it to a block, returning the final value of the ref.
     *
     * Useful to set temporary mutable variables.
     */
   extension [T](x: T) def asRef(block: Ref[T] => Unit): T = withRef(x)(block)
+
+  /** Destructures this value into multiple Refs and passes it to a block, returning the final value of the ref.
+    *
+    * Useful to set temporary mutable variables.
+    */
+  extension [T <: Product](x: T)
+    def asRefs(using mirror: Mirror.ProductOf[T])(block: Tuple.Map[mirror.MirroredElemTypes, Ref] => Unit): T =
+      withRefs(x)(block)
